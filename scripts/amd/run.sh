4045beca6fa1647eeefe33b2f2c4aa9d2cb42cb2
Michael Melesse <micmelesse@gmail.com>
2022-05-18 17:13:23 -0400

    update docker file

--- scripts/amd/run.sh
+++ scripts/amd/run.sh
@@ -1,7 +1,7 @@
 clear
 
 set -x
-set -e
+# set -e
 
 ROOT_DIR=$(pwd)
 LOG_DIR=$ROOT_DIR/log_$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
@@ -10,4 +10,6 @@ rm -rf $LOG_DIR
 mkdir -p $LOG_DIR
 chmod -R 777 $LOG_DIR
 
-bash scripts/amd/build_nightly.sh $LOG_DIR 2>&1 | tee $LOG_DIR/build_nightly.log
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+bash $SCRIPT_DIR/build_nightly.sh $LOG_DIR 2>&1 | tee $LOG_DIR/build_nightly.log
