54e47271cd60fb036845096b3cbaa1e7a2dbc887
Jason Furmanek <Jason.Furmanek@amd.com>
2022-07-19 21:29:47 +0000

    Some edits

--- tf_sig_build_dockerfiles/setup.rocm.sh
+++ tf_sig_build_dockerfiles/setup.rocm.sh
@@ -9,7 +9,14 @@ LKG_BUILD_NUM=main
 echo deb [arch=amd64 trusted=yes] $ROCM_REPO_URL $BUILD_JOB_ID $LKG_BUILD_NUM > /etc/apt/sources.list.d/rocm.list
 apt-get update
 apt-get install -y kmod
-DEBIAN_FRONTEND=noninteractive apt-get install -y rocm-dev rocm-libs
+#DEBIAN_FRONTEND=noninteractive apt-get install -y rocm-dev rocm-libs
+#MIOPENKERNELS=$( \
+#                    apt-cache search --names-only miopenkernels | \
+#                    awk '{print $1}' | \
+#                    grep -F -v . || \
+#                    true \
+#    )
+#DEBIAN_FRONTEND=noninteractive apt-get install -y ${MIOPENKERNELS}
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 echo -e "gfx900\ngfx906\ngfx908\ngfx90a\ngfx1030" >> $ROCM_PATH/bin/target.lst
